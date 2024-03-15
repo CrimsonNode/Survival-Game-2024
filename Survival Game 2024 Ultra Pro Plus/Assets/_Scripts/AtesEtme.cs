00b5439bee62be98bfb7375811d510a952a55aac
FatihEfeAydogan <efefatihaydogan@gmail.com>
2024-03-16 00:21:40 +0300

    odan baslma-6

---
+++ Survival Game 2024 Ultra Pro Plus/Assets/_Scripts/AtesEtme.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtesEtme : MonoBehaviour
+{
+    RaycastHit hit;
+    public GameObject MermiCikisNoktasi;
+    public bool AtesEdebilir;
+    float GunTimer;
+    public float TaramaHizi;
+    public ParticleSystem MuzzleFlash;
+    AudioSource SesKaynak;
+    public AudioClip AtesSesi;
+    public float Menzil;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SesKaynak = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Mouse0) && AtesEdebilir == true && Time.time > GunTimer)
+        {
+            Fire();
+            GunTimer = Time.time + TaramaHizi;
+            ;
+
+        }
+    }
+
+    void Fire()
+    {
+
+        if (Physics.Raycast(MermiCikisNoktasi.transform.position, MermiCikisNoktasi.transform.forward, out hit, Menzil))
+        {
+            MuzzleFlash.Play();
+            SesKaynak.Play();
+            SesKaynak.clip = AtesSesi;
+            Debug.Log(hit.transform.name);
+        }
+    }
+
+}
